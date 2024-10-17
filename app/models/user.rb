d238bfce86ffedf1322cb14105f8687548ffe10a
yasuhiroki <yasuhiroki.duck@gmail.com>
2024-10-17 17:40:13 +0900

    enable to POST sign_in
    - avoid CSRF protection in `.json` request
    - avoid `NoMethodError (undefined method `current_sign_in_at' for an instance of User)` error by `:trackable`
    
    The `/auth/sign_in` POST responses authentication in header.
    And use it to GET users.
    
    ```
    $ curl 'http://localhost:3010/users.json'
    {"errors":["You need to sign in or sign up before continuing."]}
    $ curl 'http://localhost:3010/users.json' -H 'authorization: Bearer eyJhY2Nlc3MtdG9rZW4iOiJtTkZ6YlN6Y01TY0J5QVdvenRIWXl3IiwidG9rZW4tdHlwZSI6IkJlYXJlciIsImNsaWVudCI6IjB0MHNiSnR2ZWl4MU1zeHpoaHJYcGciLCJleHBpcnkiOiIxNzMwMzYzOTk1IiwidWlkIjoieWFzdWhpcm9raS5kdWNrQGdtYWlsLmNvbSJ9'
    $ echo 'eyJhY2Nlc3MtdG9rZW4iOiJtTkZ6YlN6Y01TY0J5QVdvenRIWXl3IiwidG9rZW4tdHlwZSI6IkJlYXJlciIsImNsaWVudCI6IjB0MHNiSnR2ZWl4MU1zeHpoaHJYcGciLCJleHBpcnkiOiIxNzMwMzYzOTk1IiwidWlkIjoieWFzdWhpcm9raS5kdWNrQGdtYWlsLmNvbSJ9' | base64 -d
    {"access-token":"mNFzbSzcMScByAWoztHYyw","token-type":"Bearer","client":"0t0sbJtveix1MsxzhhrXpg","expiry":"1730363995","uid":"yasuhiroki.duck@gmail.com"}
    $ bin/rails c
    Loading development environment (Rails 7.1.4)
    irb(main):001> Time.at(1730363995)
    => 2024-10-31 17:39:55 +0900                                                            # default expiry is 14 days since
    irb(main):002> Time.now
    => 2024-10-17 17:46:10.210731 +0900
    
    irb(main):005> User.first.tokens
      User Load (0.2ms)  SELECT "users".* FROM "users" ORDER BY "users"."id" ASC LIMIT ?  [["LIMIT", 1]]
    =>
    {"U8H4IN9zLlhP2DmPwIW9rQ"=>{"token"=>"$2a$10$Up86Sj4I1mRaF.0.1y0On.RXE5wG50BcO6LIByPBsK/xTqVwWo1Xi", "expiry"=>1730363962},
     "0t0sbJtveix1MsxzhhrXpg"=>
      {"token"=>"$2a$10$ZEulTBtdA13e2B9sOIPJHOgTVZeA4jlvZeqi48LE6gNHM81K1.RMK",
       "expiry"=>1730364401,
       "previous_token"=>"$2a$10$uW1gRqw.ynFG8YT4DK.DEujnojcOezaZ84bFCVA40jZCAVZtd/1iC",
       "last_token"=>"$2a$10$u9GNjS9IBwg9ohaYjjMyOeLSAw6l6YcK9MvTeOhglFwUcPpa.SnGK",
       "updated_at"=>"2024-10-17T08:46:41Z"}}
    ```

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable,
-    :confirmable
+    :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
 end
